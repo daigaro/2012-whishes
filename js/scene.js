f5c28d16ead4d67701d019575459cba9cd2b5761
daigaro <daigaro@effrafax.org>
2011-12-25 11:58:43 +0100

    Random clouds
    The clouds are randomized.

--- js/scene.js
+++ js/scene.js
@@ -7,14 +7,18 @@
 	};
 	
 	window.Cloud = function(elementId, w, h){
+		var alpha = 4/10 + 4/10 * Math.random();
 		var r1 = h/2, x1 = w - r1, y1 = r1;
-		var r0 = 6 / 10 * r1, x0 = r0, y0 = h - r0;
+		var r0 = alpha * r1, x0 = r0, y0 = h - r0;
 		var x2 = (x0 + x1) / 2, y2 = (y1 - y0) / (x1 - x0) * (x2 - x0) + y0, r2 = h - y2;
+	
+		var color = "rgba(128, 128, 128, 0.75)";
+		var attributes = {fill: color, stroke: color};
 
 		var cloudPaper = Raphael(elementId, w, h);
-		cloudPaper.circle(x1,y1,r1).attr({fill: "gray", stroke: "gray"});
-		cloudPaper.circle(x0, y0, r0).attr({fill: "gray", stroke: "gray"});
-		cloudPaper.circle(x2, y2, r2).attr({fill: "gray", stroke: "gray"});
+		cloudPaper.circle(x1,y1,r1).attr(attributes);
+		cloudPaper.circle(x0, y0, r0).attr(attributes);
+		cloudPaper.circle(x2, y2, r2).attr(attributes);
 	};
 
 	window.prepareWeather = function(numberOfLayers, numberOfCloudsPersLayer){
